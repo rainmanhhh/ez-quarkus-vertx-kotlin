7f15ff7aad29d4118c3543610e0e981d83d55abd
huangwl <410488219@qq.com>
2020-04-21 09:35:10 +0800

    use PostConstruct to set CoroutineContext instead of ThreadLocal

--- src/main/kotlin/ez/vertx/VertxScope.kt
+++ src/main/kotlin/ez/vertx/VertxScope.kt
@@ -3,6 +3,7 @@ package ez.vertx
 import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.dispatcher
 import kotlinx.coroutines.CoroutineScope
+import javax.annotation.PostConstruct
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
@@ -10,9 +11,12 @@ abstract class VertxScope : CoroutineScope {
   @Inject
   lateinit var vertx: Vertx
 
-  private val cs = ThreadLocal.withInitial {
-    vertx.dispatcher()
+  private lateinit var cs: CoroutineContext
+
+  @PostConstruct
+  private fun init() {
+    cs = vertx.dispatcher()
   }
 
-  override val coroutineContext: CoroutineContext get() = cs.get()
+  override val coroutineContext: CoroutineContext get() = cs
 }
